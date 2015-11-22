f1d5c4c9f8d52be6c65d342ff9db3b2d8469c600
YsenGrimm <kloeten.wars@googlemail.com>
2015-11-22 16:54:13 +0100

    menu

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -12,13 +12,32 @@ import flixel.util.FlxMath;
  */
 class MenuState extends FlxState
 {
+
+    var startButton: FlxButton;
+    var scoreButton: FlxButton;
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
 	override public function create():Void
 	{
+        startButton = new FlxButton((FlxG.width / 2) - 40, 80, "START", startGame); 
+        add(startButton);
+
+        scoreButton = new FlxButton((FlxG.width/2) - 40, 120, "SCORE", goScore);
+        add(scoreButton);
+
 		super.create();
 	}
+
+    function startGame()
+    {
+        FlxG.switchState(new PlayState());
+    }
+
+    function goScore()
+    {
+        trace("score");
+    }
 	
 	/**
 	 * Function that is called when this state is destroyed - you might want to 
